621bfede6e6a962144d91737148e59f111abe460
Bruno <bruno.ribeiro96@hotmail.com.br>
2025-06-12 16:42:21 -0300

    feat: add pet menu

--- pets clinic/services/pet_service.py
+++ pets clinic/services/pet_service.py
@@ -100,3 +100,35 @@ def delete_pet():
     else:
         print(f'{interface.text_red('Ação cancelada')}')
     input()
+
+def menu():
+    while True:
+        interface.clear_console()
+        print('-' * 50)
+        print('Cadastro de pets'.center(50))
+        print('-' * 50)
+        print('[1] Listar Pets')
+        print('[2] Pesquisar Pet')
+        print('[3] Cadastrar Pet')
+        print('[4] Alterar Pet')
+        print('[5] Apagar Pet')
+        print('[6] Sair')
+
+        opt = input('Sua opção:')
+        if opt == '1':
+            show_pets()
+        elif opt == '2':
+            search_pet()
+        elif opt == '3':
+            insert_pet()
+        elif opt == '4':
+            update_pet()
+        elif opt == '5':
+            delete_pet()
+        elif opt == '6':
+            break
+        else:
+            interface.clear_console()
+            print(opt)
+            print(f'{interface.text_red("Opção Inválida")}')
+            input()
